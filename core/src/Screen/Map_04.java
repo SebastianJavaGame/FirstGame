40ec0f87902039c84e3a3d241f72050c4f162cb6
Sebastian1998 <sebek2088s4@tlen.pl>
2017-08-22 21:51:27 +0200

    Add collision MAP_05

--- core/src/Screen/Map_04.java
+++ core/src/Screen/Map_04.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.mygdx.game.Character;
@@ -64,8 +65,8 @@ public class Map_04 extends BaseMap {
     @Override
     public void addEntranceToMap() {
         //first entriance
-        //entriaceToMapRectangle.add(new Rectangle(500, 300, 100, 100));
-        //indexToLoadNextMap.add(4);
+        entriaceToMapRectangle.add(new Rectangle(500, 300, 100, 100));
+        indexToLoadNextMap.add(4);
         //
     }
 
